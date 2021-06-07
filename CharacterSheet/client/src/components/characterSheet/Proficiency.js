f10d2fc6caf7911a1df737ba9c5e01fbd4bcca33
cslunetta <cslunetta@outlook.com>
2021-06-07 00:04:26 -0500

    added proficiency and speed

---
+++ CharacterSheet/client/src/components/characterSheet/Proficiency.js
@@ -0,0 +1,17 @@
+import { Col, Row } from "reactstrap";
+
+export const Proficiency = () => {
+    return (
+        <Col>
+            <Row>
+                <h6>Proficiency</h6>
+            </Row>
+            <Row>
+                <div>+5</div>
+            </Row>
+            <Row>
+                <h6>Bonus</h6>
+            </Row>
+        </Col>
+    );
+};
---
+++ CharacterSheet/client/src/components/characterSheet/Speed.js
@@ -0,0 +1,17 @@
+import { Col, Row } from "reactstrap";
+
+export const Speed = () => {
+    return (
+        <Col>
+            <Row>
+                <h6>Walking</h6>
+            </Row>
+            <Row>
+                <span>35 ft.</span>
+            </Row>
+            <Row>
+                <h6>Speed</h6>
+            </Row>
+        </Col>
+    );
+};
