e93baa5cb03076959064e123c933eefd9d86bdb3
cslunetta <cslunetta@outlook.com>
2021-06-10 23:01:05 -0500

    moved passive skills to seperate module

---
+++ CharacterSheet/client/src/components/characterSheet/PassiveSkill.js
@@ -0,0 +1,17 @@
+import { Col, Row } from "reactstrap";
+
+const PassiveSkill = () => {
+    return (
+        <Col>
+            <Row>Passive Wis (Perception)</Row>
+            <Row>Passive Int (Investigation)</Row>
+            <Row>Passive Wis (Insight)</Row>
+            <Row>Darkvision 60ft.</Row>
+            <Row>
+                <p>Senses</p>
+            </Row>
+        </Col>
+    );
+};
+
+export default PassiveSkill;
