6c869cb70cbbe7db7f6613363752ea3934528137
cslunetta <cslunetta@outlook.com>
2021-06-11 20:04:11 -0500

    moved skills to seperate module

---
+++ CharacterSheet/client/src/components/characterSheet/Skills.js
@@ -0,0 +1,56 @@
+import { Table } from "reactstrap";
+
+const Skills = () => {
+    return (
+        <Table>
+            <thead>
+                <tr>
+                    <th>Prof</th>
+                    <th>Mod</th>
+                    <th>Skill</th>
+                    <th>Bonus</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr>
+                    <td> O</td>
+                    <td>DEX</td>
+                    <td>Skill Name</td>
+                    <td>+2</td>
+                </tr>
+                <tr>
+                    <td> O</td>
+                    <td>DEX</td>
+                    <td>Skill Name</td>
+                    <td>+2</td>
+                </tr>
+                <tr>
+                    <td> O</td>
+                    <td>DEX</td>
+                    <td>Skill Name</td>
+                    <td>+2</td>
+                </tr>
+                <tr>
+                    <td> O</td>
+                    <td>DEX</td>
+                    <td>Skill Name</td>
+                    <td>+2</td>
+                </tr>
+                <tr>
+                    <td> O</td>
+                    <td>DEX</td>
+                    <td>Skill Name</td>
+                    <td>+2</td>
+                </tr>
+                <tr>
+                    <td> O</td>
+                    <td>DEX</td>
+                    <td>Skill Name</td>
+                    <td>+2</td>
+                </tr>
+            </tbody>
+        </Table>
+    );
+};
+
+export default Skills;
