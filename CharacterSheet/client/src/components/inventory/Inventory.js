09f9423921a528b2cefc6fb14739e5cd1e230eee
cslunetta <cslunetta@outlook.com>
2021-06-18 23:53:47 -0500

    added navigation for different inventories

--- CharacterSheet/client/src/components/inventory/Inventory.js
+++ CharacterSheet/client/src/components/inventory/Inventory.js
@@ -1,8 +1,35 @@
+import { Nav, NavItem, NavLink } from "reactstrap";
+
 const Inventory = () => {
+    const AvailableInventories = () => {
+        return (
+            <>
+                <NavItem>
+                    <NavLink>Character Backback</NavLink>
+                </NavItem>
+                <NavItem>
+                    <NavLink>Quiver</NavLink>
+                </NavItem>
+                <NavItem>
+                    <NavLink>Bag of holding</NavLink>
+                </NavItem>
+            </>
+        );
+    };
+
     return (
         <>
-            <h1>Inventory</h1>
-            <h2>Storage Types</h2>
+            <h1>Inventory Management</h1>
+            <div className="inventorys">
+                <Nav tabs>
+                    {/* map over inventories available for the user in the campaign */}
+                    <AvailableInventories />
+                </Nav>
+            </div>
+            <div>
+
+            </div>
+            {/*<h2>Storage Types</h2>
             <p>Player Character</p>
             <p>NPC</p>
             <p>Storage</p>
@@ -13,7 +40,7 @@ const Inventory = () => {
             <p>stacked equipement or listed individually</p>
             <p>if stackable qty</p>
             <p>description</p>
-            <p>does item effect stats / abilities / add a feature</p>
+            <p>does item effect stats / abilities / add a feature</p> */}
         </>
     );
 };
